456180b823efb5fcb5df71d2207d7cff5e700552
generic-placeholder-name <48155874+generic-placeholder-name@users.noreply.github.com>
2023-07-24 14:11:39 +0700

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -1,5 +1,5 @@
 #pragma GCC optimize("Ofast")
-#pragma GCC optimization("unroll-loops, no-stack-protector")
+#pragma GCC optimize("unroll-loops")
 #pragma GCC target("avx,avx2,fma")
 
 #include <bits/stdc++.h>
@@ -11,7 +11,7 @@ using namespace std;
 using namespace __gnu_pbds;
 using namespace __gnu_cxx;
 
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
 #define fi first
 #define se second
@@ -19,7 +19,6 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define eb emplace_back
 #define mp make_pair
 #define gcd __gcd
-#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define rep(i, n) for (int i=0; i<(n); i++)
 #define rep1(i, n) for (int i=1; i<=(n); i++)
 #define all(x) (x).begin(), (x).end()
@@ -43,5 +42,6 @@ using ordered_set=tree<T, null_type, cmp, rb_tree_tag, tree_order_statistics_nod
 typedef trie<string, null_type, trie_string_access_traits<>, pat_trie_tag, trie_prefix_search_node_update> pref_trie;
 
 int32_t main() {
-    fastio;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 }
